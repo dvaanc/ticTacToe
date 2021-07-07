054736c5c0cd44410b6018a3885813535b379014
Danny C <dannyc52521@gmail.com>
2021-07-07 12:34:12 +1000

    Minor updates

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 'use strict';
+//player handler 
+const player = (side) => {
+  this.side = side;
+  const getSide = () => side;
 
-const gameBoard = (function(){
+  return { getSide };
+}
+
+//game board handler
+const gameBoard = (() => {
   const _board = 
   ["", "", "",
    "", "", "",
@@ -11,4 +19,27 @@ const gameBoard = (function(){
   };
     
   return { resetBoard };
+})();
+
+
+// dom handler
+const ui = (() => {
+  // cache dom
+  const cellsEl = document.querySelectorAll(".cell");
+  const restartEl = document.querySelector("#restart");
+  const messageEl = document.querySelector(".message");
+  // set message
+  // update cell on player click
+  // reset all cells to ""
+
+
+})();
+
+
+
+// game logic
+const game = (() => {
+  const player1 = player("X");
+  const player2 = player("O");
+
 })();
